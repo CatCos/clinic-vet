d8f3823f5a7671bdfc74180b50a60b8181149184
CatCos <catarinacosta217@gmail.com>
2016-07-29 17:37:51 +0100

    Fix routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,19 +5,23 @@ const router = express.Router();
 const models = require("../models");
 const users = require("../handlers/users.js");
 
+// Get the information of all users
 router.get('/', (req, res) => {
-  let response = users.all()
-
-  res.json(response)
+  let response = users.all(res)
 
 });
 
-router.post('/new', (req, res) => {
+// Add new user
+router.get('/new', (req, res) => {
   let response = users.new(req);
 
-
-
 });
+
+// Get information of a specific user
+router.get('/:id', (req, res) => {
+  let response = users.find(req.params.id, res)
+
+})
 
 
 module.exports = router;
